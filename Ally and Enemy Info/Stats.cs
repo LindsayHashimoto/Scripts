4030ef4c2193c0802e371c10acc8a025cf5d9b8d
LindsayHashimoto <46505110+LindsayHashimoto@users.noreply.github.com>
2019-04-01 15:28:21 -0400

    Update Stats.cs

--- Ally and Enemy Info/Stats.cs
+++ Ally and Enemy Info/Stats.cs
@@ -10,26 +10,32 @@ public class Stats : MonoBehaviour {
     public int m_level;
     public int m_exp;
 
-    public int m_hp;
     public int m_atk;
     public int m_def;
     // Determines the turn order
     public int m_initiative;
     public bool m_isEnemy;
 
-    public HealthManager m_healthManager; 
+    public HealthManager m_healthManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_healthManager.SetMaxHealth(10);
+    }
 
     public Stats(string a_name, int a_level, int a_exp, int a_hp, int a_atk, int a_def, bool a_isEnemy)
     {
         m_entityName = a_name;
         m_level = a_level;
         m_exp = a_exp;
-        m_hp = a_hp;
+        m_healthManager.SetMaxHealth(a_hp); 
         m_atk = a_atk;
         m_def = a_def;
         m_isEnemy = a_isEnemy;
         m_initiative = 0; 
     }
+
 
     public void levelUp()
     {
