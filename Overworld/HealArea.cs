0f5f97f9ef8d4807f72be94dfaf0437fad44b2ac
LindsayHashimoto <46505110+LindsayHashimoto@users.noreply.github.com>
2019-06-14 12:41:37 -0400

    Player can heal themselves if they enter the heal area

---
+++ Overworld/HealArea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealArea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D a_other)
+    {
+        if (a_other.tag == "Player")
+        {
+            SceneManagerScript sm = a_other.GetComponentInParent<SceneManagerScript>();
+            GameObject allies = sm.transform.Find("Allies").gameObject;
+            HealthManager[] allyHealth = allies.GetComponentsInChildren<HealthManager>();
+            
+            foreach (HealthManager personToHeal in allyHealth)
+            {
+                personToHeal.FullyHeal(); 
+            }
+        }
+    }
+}
